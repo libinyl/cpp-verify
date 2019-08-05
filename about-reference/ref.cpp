9901bb9e4fab9f34a5ca64b45c0f74f7af5dc872
libinyl <libin152@outlook.com>
2019-08-05 22:54:43 +0800

    update "comparation between pass-by-value & pass-by-reference"

--- about-reference/ref.cpp
+++ about-reference/ref.cpp
@@ -1,15 +1,41 @@
+#include <chrono>
+#include <iostream>
 class A
 {
-    int v = 1;
+    char buf[1024 * 1024] = {'\0'};
+    char buf1[1024 * 512] = {'\0'};
 };
-
-void callee(A& a)
+A callee_val(A a)
 {
-
+    return a;
+}
+A &callee_ref(A &a)
+{
+    return a;
 }
 
-void caller()
+void caller(bool pass_by_value)
 {
     A _a;
-    callee(_a);
+    A b = pass_by_value ? callee_val(_a) : callee_ref(_a);
+}
+
+
+using namespace std;
+using namespace chrono;
+int main()
+{
+    auto s1 = system_clock::now();
+    caller(true);
+    auto e1 = system_clock::now();
+    auto t1 = duration_cast<nanoseconds>(e1 - s1);
+
+    auto s2 = system_clock::now();
+    caller(false);
+    auto e2 = system_clock::now();
+    auto t2 = duration_cast<nanoseconds>(e2 - s2);
+
+    cout << "pass by value 耗时 " << t1.count() << " ns;" << endl;
+    cout << "pass by reference 耗时 " << t2.count() << " ns;" << endl;
+    return 0;
 }
